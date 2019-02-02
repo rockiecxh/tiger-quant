bdcfc4a6f689082b73aef201f30071fd324f66c1
Andrew <wj.andrew@yahoo.com>
2019-02-02 20:48:15 +0800

    双Y轴显示

--- demo2.py
+++ demo2.py
@@ -9,6 +9,7 @@ from tigeropen.quote.quote_client import QuoteClient
 from tigeropen.tiger_open_config import TigerOpenClientConfig
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 
 
 def get_client_config():
@@ -45,27 +46,39 @@ if __name__ == '__main__':
     config = get_client_config()
     quant_client = QuoteClient(config)
 
-    stocks = ['QQQ', 'KWEB']
+    stocks = ['QQQ', 'TLT']
     data = quant_client.get_bars(stocks)
 
     years = mdates.YearLocator()  # every year
     months = mdates.MonthLocator()  # every month
     yearsFmt = mdates.DateFormatter('%Y')
 
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-    for stock in stocks:
-        y1 = data.loc[(data["symbol"] == stock)]
-        x_time = timestamp_2_str(y1['time'].values)
-        ax.plot(x_time, y1['close'], color=random_color(), label=stock)
+    # ax1 = None
+    # for index, stock in stocks:
+    y1 = data.loc[(data["symbol"] == stocks[0])]
+    x_time = timestamp_2_str(y1['time'].values)
+    ax.plot(x_time, y1['close'], color='red', label=stocks[0])
 
-    ax.xaxis.set_major_locator(years)
-    ax.xaxis.set_major_formatter(yearsFmt)
-    ax.xaxis.set_minor_locator(months)
+    y2 = data.loc[(data["symbol"] == stocks[1])]
+    ax2 = ax.twinx()
+    ax2.plot(x_time, y2['close'], color='green', label=stocks[1])
+
+    # mondays = mdates.WeekdayLocator(mdates.MONDAY)  # major ticks on the mondays
+    # alldays = mdates.DayLocator()  # minor ticks on the days
+    # weekFormatter = mdates.DateFormatter('%Y-%m-%d')  # e.g., 2018-09-12; Jan 12
+    # dayFormatter = mdates.DateFormatter('%d')  # e.g., 12
+
+    # ax.xaxis.set_major_locator(mondays)
+    # ax.xaxis.set_minor_locator(alldays)
+    # ax.xaxis.set_major_formatter(weekFormatter)
+    # ax.xaxis_date()
     ax.autoscale_view()
-    ax.fmt_xdata = mdates.DateFormatter('%Y-%m')
-    # ax.fmt_ydata = price
-    ax.grid(True)
+    # ax.grid(True)
+    # plt.yticks([])
     plt.legend()
     plt.show()
 
