304a34589155edb9ecfaeec65ef61f0079e4d7b8
Andrew <wj.andrew@yahoo.com>
2019-02-19 16:09:34 +0800

    将配置文件分离

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# tiger-quant
+# tiger-quant 是根据老虎量化平台的数据来做一些简单的指数分析案例
 
+##1、如何配置
+### 新建配置文件
+~/config/env.properties
+####老虎量化平台ID与账号
+~~~
+tiger_id=xxx
+tiger_account=xxx
+~~~
+
+####老虎开放平台私钥路径
+~/.ssh/tigerbroker_rsa_private_key.pem
+
+
+##2、如何使用
 ####获取指定代码的相关系数并以热力图的方式展现
-correlation_coefficient.py
+python correlation_coefficient.py
 
 ####K线叠加
-kline_overlay.py
+python kline_overlay.py
--- tiger/config.py
+++ tiger/config.py
@@ -4,6 +4,7 @@ from tigeropen.common.consts import Language
 from tigeropen.common.util.signature_utils import read_private_key
 from tigeropen.quote.quote_client import QuoteClient
 from tigeropen.tiger_open_config import TigerOpenClientConfig
+from properties.p import Property
 
 
 def get_client_config():
@@ -11,11 +12,17 @@ def get_client_config():
     https://www.itiger.com/openapi/info 开发者信息获取
     :return:
     """
+    # Load tiger account information
+    prop = Property()
+    prop_loader = prop.load_property_files(os.path.expanduser('~/config/env.properties'))
+    tiger_id = prop_loader.get('tiger_id')
+    tiger_account = prop_loader.get('tiger_account')
+
     is_sandbox = False
     client_config = TigerOpenClientConfig(sandbox_debug=is_sandbox)
     client_config.private_key = read_private_key(os.path.expanduser('~/.ssh/tigerbroker_rsa_private_key.pem'))
-    client_config.tiger_id = '20150138'
-    client_config.account = '20190130215629871'
+    client_config.tiger_id = tiger_id
+    client_config.account = tiger_account
     client_config.language = Language.en_US
 
     return client_config
