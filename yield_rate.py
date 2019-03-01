071cfc9ec26a9a7de68dce750b578af83b1e2d6d
Andrew <wj.andrew@yahoo.com>
2019-03-01 14:32:55 +0800

    合并累计收益图

--- yield_rate.py
+++ yield_rate.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import dates
 from tigeropen.common.consts import BarPeriod
 
-from lib.chart import subplot_num
 from lib.date import timestamp_2_date, get_today, date_delta
 from lib.quant import log_yield_rate
 from tiger.config import get_bars_from_cache, get_quote_client
+
+
+plt.rc('font', family='simhei')
 
 
 def yield_rate_plot(data: pd.DataFrame, stocks: []):
@@ -19,37 +23,26 @@ def yield_rate_plot(data: pd.DataFrame, stocks: []):
     time = data.loc[(data["symbol"] == stocks[0])]['time']
     time = timestamp_2_date(time.tolist())
 
-    m, n = subplot_num(len(stocks))
-    fig = plt.figure()
-    idx = 1
+    df = pd.DataFrame(index=time[1:])
 
     for stock in stocks:
         stock_data = data.loc[(data['symbol'] == stock)]
 
-        normal_return_rate = list(stock_data['close'].pct_change().dropna())
         ln_return_rate = log_yield_rate(list(stock_data['close']))
+        df[stock] = np.cumsum(ln_return_rate)
 
-        stock_normal = '{0}_normal'.format(stock)
-        stock_log = '{0}_log'.format(stock)
-
-        df = pd.DataFrame(index=time[1:])
-        df[stock_normal] = np.cumsum(normal_return_rate)
-        df[stock_log] = np.cumsum(ln_return_rate)
-
-        ax = fig.add_subplot(m, n, idx)
-        ax.plot(df)
-        ax.legend([stock_normal, stock_log])
-
-        idx += 1
-
+    g = sns.lineplot(data=df)
     # X轴刻度设置
-    # g.format_xdata = dates.AutoDateFormatter(dates.MonthLocator())
+    g.format_xdata = dates.AutoDateFormatter(dates.MonthLocator())
+
+    plt.legend()
+    plt.title('累计收益率')
     plt.show()
 
 
 if __name__ == '__main__':
     quote_client = get_quote_client()
-    stocks = ['SCHB', 'QQQ', 'TLT', 'IAU']
+    stocks = ['SCHB', 'QQQ', 'TLT', 'IAU', 'WTI']
 
     data = get_bars_from_cache(quote_client, symbols=stocks, period=BarPeriod.DAY,
                                begin_time=date_delta(-52 * 5), end_time=get_today())
